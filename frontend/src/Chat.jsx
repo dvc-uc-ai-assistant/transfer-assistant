5d394428f596d692a2f7c2cd736cec8ba1089093
courageousstrawberry <elech2a@gmail.com>
2026-03-18 13:44:45 -0700

    fix refresh issues

--- frontend/src/Chat.jsx
+++ frontend/src/Chat.jsx
@@ -1,12 +1,9 @@
 import NexaChat from "./NexaChat";
-import { useLocation } from "react-router-dom";
 
 export default function Chat() {
-  const location = useLocation();
-
   return (
     <div className="chat-page">
-      <NexaChat key={location.key} />
+      <NexaChat />
     </div>
   );
 }
